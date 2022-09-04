c44704b22246b7860df665a9eabfc2ffdb4a4f42
BogdanKorolev <bkorolevjob@gmail.com>
2022-09-04 16:03:00 +0300

    FinalTest folder: created || Dotnet Console: created || Task solution: string array created via manual input of strings.

---
+++ FinalTest/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Написать программу,
+которая из имеющегося массива строк формирует массив из строк,
+длина которых меньше или равна трем (3) символам.
+*/
+
+string[,] inputTable = new string [1,4];
+
+Console.WriteLine("Введите первую строку: ");
+inputTable[0,0] = Console.ReadLine();
+
+Console.WriteLine("Введите вторую строку: ");
+inputTable[0,1] = Console.ReadLine();
+
+Console.WriteLine("Введите третью строку: ");
+inputTable[0,2] = Console.ReadLine();
+
+Console.WriteLine("Введите четверту строку: ");
+inputTable[0,3] = Console.ReadLine();
+
+
+
+
